31e47e92632eef35f5c6a304deae8b5944c5ba6c
Salar <ahmadsalar0808@gmail.com>
2026-05-29 12:25:28 +0500

    add supabase connection

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Supabase Setup
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const SUPABASE_URL = 'https://bafpnqleaivhlbtbvufg.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_dyg3P9bHZkwRn7_bErLySw_lGPgdGfc'
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+
 // ===== DATA =====
 const posts = [
   {
